c6ac0580add76da82d8d7799b220f76f81ec0da3
Dominic Yegon <dyegon@meliora.tech>
2023-11-30 20:28:03 +0300

    Added login page

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/account/login/login.component';
+import { ChatComponent } from './components/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "",
+        component: ChatComponent
+    },
+    {
+        path: "**",
+        redirectTo: "/"
+    }
+];
